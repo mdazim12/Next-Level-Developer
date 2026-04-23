8f88a315737614211563e8efe4f17adc3d078488
mdazim12 <azimuddin807@gmail.com>
2026-04-23 21:22:00 +0600

    M-0 Module 02 map

--- Milestone 0/Module 02/2.1 map.js
+++
@@ -1,7 +0,0 @@
-
-    const obj = {
-        nextLevel : { courseId : 'level2' },
-        "Programing Hero" : {courseId : "level1"}
-    }
-
-    console.log(obj)
---
+++ Milestone0/Module02/map.js
@@ -0,0 +1,31 @@
+
+    // const obj = {
+    //     nextLevel : { courseId : 'level2' },
+    //     true : {courseId : "level1"},
+    // }
+
+    // // console.log(obj.nextLevel["Programing Hero"]);
+    // console.log(obj["true"]);
+
+    // const obj = {};
+    // obj.nextLevel = { courseId : "level2"};
+    // obj[course] = {courseId : "level1"};
+
+    // console.log(obj["[object Object]"])
+
+
+    const course = {name : "Programming Hero"}
+    const course2 = {name : "Next Level Web Development"}
+
+
+    const map = new Map();
+
+    map.set(true, "programing Hero")
+
+    console.log(map)
+
+
+    
+
+ 
+  
