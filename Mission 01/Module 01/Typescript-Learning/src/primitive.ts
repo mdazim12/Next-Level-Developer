3f9fba6253016a207f69da033176cdb3fe2b3653
mdazim12 <azimuddin807@gmail.com>
2026-05-02 01:22:51 +0600

    primitive data

---
+++ Mission 01/Module 01/Typescript-Learning/src/primitive.ts
@@ -0,0 +1,29 @@
+//JS - TS
+// string , number , boolean , undefined. null
+
+// TS : never , unknown, void
+
+let userName: string = "azim123";
+
+// userName = "Zaim123"
+
+// userName = 123
+
+let useId = 123
+
+// useId = "123"
+
+userName.length;
+
+useId.toFixed()
+
+let isAdmin = false ;
+
+// isAdmin = 'azim'
+isAdmin = true;
+
+let x = undefined;
+
+x = 5;
+x = "five"
+x = true;
