ea2a9ad8bd9634b2494bb4b4561ccb4638a2ad1c
mdazim12 <azimuddin807@gmail.com>
2026-05-02 01:50:02 +0600

    nonprimitive array

---
+++ Mission 01/Module 01/Typescript-Learning/src/nonPrimitive.ts
@@ -0,0 +1,34 @@
+// array, object
+
+//ts -- tuple
+
+let bazarlist : string[] = ['eggs' , 'milk' , 'sugar'];
+
+// bazarlist.push(12)
+// bazarlist.push(true)
+
+
+
+let mixedArr : (string | number)[] = ['eggs', 12 , 'milk' , 1, 'sugar', 2];
+
+mixedArr.push('azim')
+mixedArr.push(123)
+// mixedArr.push(true)
+
+
+
+
+let coordinates: [number, number] = [20,30]
+// let coordinates: [number, number] = [20,30,50]
+
+let couple : [string, string]= ['husband' , 'wife'];
+// let couple : string= ['husband' , 'wife'];
+
+
+
+let nameAndRoll : [string, number] = ['azim', 84]
+// nameAndRoll[0] = 76;
+
+
+let destination: [string,string,number] = ['Dhaka' , 'Chattrogram' , 3];
+
