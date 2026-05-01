c55aad92ebdc6c085b7cb465d675ea1404645047
mdazim12 <azimuddin807@gmail.com>
2026-05-02 01:06:21 +0600

    start typescript

---
+++ Mission 01/Module 01/Typescript-Learning/dist/test.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const course = "next level developer";
+console.log(course);
+//# sourceMappingURL=test.js.map
---
+++ Mission 01/Module 01/Typescript-Learning/src/test.ts
@@ -0,0 +1,3 @@
+const course: string = "next level developer";
+
+console.log(course)
