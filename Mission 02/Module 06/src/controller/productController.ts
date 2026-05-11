88956215a168ddd189891c5158f7ba8fb0f5f8f4
mdazim12 <azimuddin807@gmail.com>
2026-05-12 00:45:53 +0600

    Reading Files With fs module

--- Mission 02/Module 06/src/controller/productController.ts
+++ Mission 02/Module 06/src/controller/productController.ts
@@ -1,4 +1,5 @@
 import type { IncomingMessage, ServerResponse } from "http";
+import { readProduct } from "../service/productService";
 
 export const productController = (req: IncomingMessage, res: ServerResponse) => {
 
@@ -6,16 +7,33 @@ export const productController = (req: IncomingMessage, res: ServerResponse) => 
     const url = req.url;
     const method = req.method
 
+    const urlParts = url?.split('/');
+
+    const id = urlParts && urlParts[1] === "products" ? Number(urlParts[2] ) : null;
+
+    // console.log("This is the accutal id : " , id);
+
+
     if (url === "/products" && method === "GET") {
 
-        const products = [
-            {
-                id : 1,
-                name : "product 01"
-            }
-        ]
+        // const products = [
+        //     {
+        //         id : 1,
+        //         name : "product 01"
+        //     }
+        // ]
+
+        const products = readProduct();
+
+        readProduct();
 
         res.writeHead(200, { "content-type": "application/json" });
-        res.end(JSON.stringify({ message: "This is products route AZIM" , data :{products} }));
+        res.end(JSON.stringify({ message: "Product retrived succesfully" ,
+        data :products }));
     }
+    // else if (method === "GET" && id != null) {
+
+    // }
+
+    
 }
---
+++ Mission 02/Module 06/src/service/productService.ts
@@ -0,0 +1,14 @@
+import * as path from "path";
+import * as fs from "fs";
+
+const filePath = path.join(process.cwd(), './src/database/db.json');
+
+
+export const readProduct = ()=> {
+    
+    const produtcs = fs.readFileSync(filePath, 'utf-8');
+    // console.log(produtcs.toString());
+    console.log(JSON.parse(produtcs));
+    return JSON.parse(produtcs);
+
+}
