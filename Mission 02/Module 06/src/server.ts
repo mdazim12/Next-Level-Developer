768e7742c6ca4377084482d8c47f954c1be1ebc9
mdazim12 <azimuddin807@gmail.com>
2026-05-11 12:55:10 +0600

    handling http request and response

--- Mission 02/Module 06/src/server.ts
+++ Mission 02/Module 06/src/server.ts
@@ -1,12 +1,34 @@
-import { createServer, IncomingMessage, Server } from "http";
+    import { createServer, IncomingMessage, Server } from "http";
 
-const server : Server = createServer((req : IncomingMessage,res) => {
-    console.log(req);
-})
+    const server : Server = createServer((req : IncomingMessage,res) => {
+        // console.log(req.url);      ////   '/' , '/user ,'/product'
+        // console.log(req.method);  ///   'GET' , 'POST' , 'DELETE'
+
+        const url = req.url;
+        const method = req.method
+
+        if(url === '/' && method === 'GET') {
+            // console.log('This is root route');
+            res.writeHead(200, {"content-type" : "application/json"});
+            res.end(JSON.stringify({message : "This is root route"}));
+        } else if(url?.startsWith('/products') ) {
+            res.writeHead(200, {"content-type" : "application/json"});
+            res.end(JSON.stringify({message : "This is products route"}));
+        }
+        
+        
+        
+        
+        else{
+        res.writeHead(404, {"content-type" : "application/json"});
+            res.end(JSON.stringify({message : "route not found"}));
+        }
+
+    })
 
 
-server.listen(6000, () => {
-    console.log('Server is running on the port 6000');
-});
+    server.listen(3000, () => {
+    console.log("Server running on port 3000");
+    });
 
 
