41ee8eb0f7ac10659d75f33d7ec8d51a2f8ec61f
mdazim12 <azimuddin807@gmail.com>
2026-05-12 15:10:37 +0600

    Perfecting PUT Method

--- Mission 02/Module 06/src/controller/productController.ts
+++ Mission 02/Module 06/src/controller/productController.ts
@@ -84,4 +84,41 @@ export const productController = async (req: IncomingMessage, res: ServerRespons
     }
 
 
+    else if(method === "PUT" && id !== null ) {
+        const body = await parseBody(req)
+        const products = readProduct();
+
+        const index = products.findIndex((p:IProduct) => p.id === id )
+
+        console.log(index);
+
+        if(index <=0) {
+            res.writeHead(404, {"content-type" : "application/json"});
+            res.end(
+
+                JSON.stringify({
+                    message : "Product Not Found",
+                    data : null
+                })
+            )
+        } 
+        
+        // console.log(products[index]);
+        products[index] =  {id : products[index].id,  ...body };
+
+        insertProduct(products);
+
+        res.writeHead(200, {"content-type" : "application/json"});
+        res.end (
+            JSON.stringify({
+                message: "Product Updated sucessfully",
+                data : products[index]
+            })
+        )
+
+  
+
+    }
+
+
 }
