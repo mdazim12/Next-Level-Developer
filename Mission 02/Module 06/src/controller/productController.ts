8bda74ad5940e6c6df94fb2370a2bb11a86b99a7
mdazim12 <azimuddin807@gmail.com>
2026-05-12 17:33:02 +0600

    Executing DELETE Method Operations

--- Mission 02/Module 06/src/controller/productController.ts
+++ Mission 02/Module 06/src/controller/productController.ts
@@ -43,6 +43,16 @@ export const productController = async (req: IncomingMessage, res: ServerRespons
         const product = products.find((p: IProduct) => p.id === id)
         // console.log(product);
 
+        if(!product) {
+            res.writeHead(404, {'content-type' : 'application/json'});
+            res.end(
+                JSON.stringify({
+                    message : 'Product not found',
+                    data : product
+                })
+            )
+        }
+
         res.writeHead(200, { "content-type": "application/json" });
         res.end(JSON.stringify({
             message: "Product retrived succesfully",
@@ -115,10 +125,42 @@ export const productController = async (req: IncomingMessage, res: ServerRespons
                 data : products[index]
             })
         )
+    }
 
-  
+
+    else if(method  ==="DELETE" && id!== null  ) {
+        const products = readProduct();
+
+        const index = products.findIndex((p: IProduct) => p.id === id);
+
+        if(index < 0) {
+            res.writeHead(404, {"content-type" : "application/json" });
+            res.end (
+                JSON.stringify({
+                    message : "Product not found",
+                    data : null,
+                }),
+
+
+            );
+        }
+        // const arr = ["1" ,"2" ,"3," , "4"]
+        products.splice(index,1);
+        // console.log(products);
+
+        insertProduct(products)
+
+        res.writeHead(200, {'content-type' : 'application/json'});
+        res.end(
+            JSON.stringify({
+                message : 'Product deleted sucessfully',
+                data : null
+            })
+        )
 
     }
+
+   
 
 
 }
