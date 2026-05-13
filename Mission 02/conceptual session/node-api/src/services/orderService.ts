f270b93280b1283855d481ef43f379d6e2a18353
mdazim12 <azimuddin807@gmail.com>
2026-05-13 23:53:02 +0600

    product added successfully

--- Mission 02/conceptual session/node-api/src/services/orderService.ts
+++ Mission 02/conceptual session/node-api/src/services/orderService.ts
@@ -1,12 +1,13 @@
 import path from "path";
 import fs from "fs/promises";
 import type { Order } from "../type";
+import { stringify } from "querystring";
 
 const DB_PATH = path.join(process.cwd(), "db", "data.json")
 
 class OrderService {
     // readDATE ,writeDATA
-   private  async readData(): Promise<Order[]>  {
+    private async readData(): Promise<Order[]> {
         try {
             const data = await fs.readFile(DB_PATH, "utf-8")
             return JSON.parse(data)
@@ -17,8 +18,15 @@ class OrderService {
     }
 
 
-    private  async writeData(data:Order){
-        await fs.writeFile(DB_PATH, JSON.stringify(data))
+    // private async writeData(data: Promise<Order[]>) {
+    //     await fs.writeFile(DB_PATH, JSON.stringify(data, null, 2));
+    // }
+
+    private async writeData(data: Order[]): Promise<void> {
+        await fs.writeFile(
+            DB_PATH,
+            JSON.stringify(data, null, 2)
+        );
     }
 
 
@@ -31,12 +39,30 @@ class OrderService {
 
 
     /// CREATE DATA
+    async create(order: Omit<Order, "id">) {
 
-    
+        const data = await this.readData();
 
+        const newOrder = {
+            id: String(Math.floor(Math.random() * 100)),
+            ...order
+            
+        }
+
+        data.push(newOrder)
+
+        await this.writeData(data)
+    }
 
 
 }
 
 const orderService = new OrderService();
- 
+
+await orderService.create({
+    customare: " joan",
+    food: "pizza",
+    price: 1600,
+    quantity: 4
+
+})
