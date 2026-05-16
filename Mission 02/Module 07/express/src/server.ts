b2c008dd2f84c78f67773ac228ad1804cb061a2a
mdazim12 <azimuddin807@gmail.com>
2026-05-16 15:24:32 +0600

    7-5 Executing Pool and creating tables

--- Mission 02/Module 07/express/src/server.ts
+++ Mission 02/Module 07/express/src/server.ts
@@ -3,7 +3,7 @@ import {Pool} from "pg"
 
 
 const app : Application = express()
-const port = 5000
+const port = 3000
 
 app.use(express.json());
 app.use(express.text());
@@ -15,6 +15,29 @@ const pool = new Pool({
 })
 
 
+const initDB = async()=> {
+    try {
+        await pool.query(`
+            CREATE TABLE IF NOT EXISTS users (
+                id SERIAL PRIMARY KEY,
+                name VARCHAR(20),
+                email VARCHAR(20) NOT NULL,
+                password VARCHAR(20) NOT NULL,
+                is_active BOOLEAN DEFAULT true,
+                age INT,
+
+                created_at TIMESTAMP DEFAULT NOW(),
+                updated_at TIMESTAMP DEFAULT NOW()
+            )
+            `)  
+            console.log("Database connected successfully");
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+initDB();
+
 app.get('/', (req : Request , res : Response) => {
 //   res.send('Exprss server!')
     res.status(200).json({
@@ -22,7 +45,7 @@ app.get('/', (req : Request , res : Response) => {
         "author" : "Next level"
     });
 
-})
+})          
 
 app.post('/' , async(req : Request,res : Response)=> {
     // console.log(req.body);
