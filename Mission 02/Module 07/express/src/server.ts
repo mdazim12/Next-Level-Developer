8b6e3b9a33ae6155c3eee46362ce4e2db05682a4
mdazim12 <azimuddin807@gmail.com>
2026-05-17 13:11:37 +0600

    7-9 Delete User with Delete Method

--- Mission 02/Module 07/express/src/server.ts
+++ Mission 02/Module 07/express/src/server.ts
@@ -137,7 +137,13 @@ app.put('/api/users/:id', async (req: Request, res: Response) => {
 
     try {
         const result = await pool.query(`
-        UPDATE users SET name = $1,password = $2, age = $3, is_active = $4 
+        UPDATE users
+        SET 
+        name = COALESCE($1, name),
+        password = COALESCE($2, password),
+        age = COALESCE($3, age), 
+        is_active = COALESCE($4, is_active)
+
         WHERE id = $5 RETURNING *
     `,
             [name, password, age, is_active, id])
@@ -161,6 +167,37 @@ app.put('/api/users/:id', async (req: Request, res: Response) => {
             message: error.message,
             error: error
         })
+    }
+});
+
+
+app.delete("/api/users/:id" , async(req: Request, res: Response)=> {
+    const {id} = req.params
+    try {
+        const result = await pool.query(`
+        DELETE FROM users WHERE id = $1
+            `,
+            [id]
+        )
+
+        if(result.rowCount === 0) {
+            res.status(404).json({
+                success : false,
+                message : "User Not Found",
+            })
+        }
+
+        res.status(200).json({
+            success : true,
+            message : 'User Deleted Sucessfully!',
+            data : {}
+        })
+    } catch (error : any) {
+        res.status(500).json({
+            success : false,
+            message : error.message,
+            error : error
+        });
     }
 });
 
