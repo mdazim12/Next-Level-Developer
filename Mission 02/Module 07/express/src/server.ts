ef58c7f79741629e004269acf3432cb249fb14ce
mdazim12 <azimuddin807@gmail.com>
2026-05-17 12:46:38 +0600

    7-8 Update User with the PUT method

--- Mission 02/Module 07/express/src/server.ts
+++ Mission 02/Module 07/express/src/server.ts
@@ -106,7 +106,7 @@ app.get("/api/users/:id", async (req: Request, res: Response) => {
         );
 
         if (result.rows.length === 0) {
-            res.status(500).json({
+            res.status(404).json({
                 success: false,
                 message: "Users Not Found ! ",
                 data: {},
@@ -126,6 +126,43 @@ app.get("/api/users/:id", async (req: Request, res: Response) => {
         })
     }
 })
+
+
+
+app.put('/api/users/:id', async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const { name, password, age, is_active } = req.body;
+    // console.log("Id :" ,id);
+    // console.log({name,password,age,is_active});
+
+    try {
+        const result = await pool.query(`
+        UPDATE users SET name = $1,password = $2, age = $3, is_active = $4 
+        WHERE id = $5 RETURNING *
+    `,
+            [name, password, age, is_active, id])
+       
+           if(result.rows.length === 0) {
+                res.status(404).json({
+                    success : false,
+                    message : "User Not Found For Update!",
+                    data : {},
+                })
+           } 
+
+        res.status(200).json({
+            success: true,
+            message: "User updated successfully!",
+            data: result.rows[0]
+        });
+    } catch (error: any) {
+        res.status(500).json({
+            success: false,
+            message: error.message,
+            error: error
+        })
+    }
+});
 
 
 
