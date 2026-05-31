3e98aeefbc7dde065a21dabd7753c6cacffbc1cb
mdazim12 <azimuddin807@gmail.com>
2026-05-31 14:48:55 +0600

    9-4 Recap the auth middleware and setting the namespace

--- Mission 02/Module 08/express/src/middleware/auth.ts
+++ Mission 02/Module 08/express/src/middleware/auth.ts
@@ -1,5 +1,5 @@
 import type { NextFunction, Request, Response } from "express";
-import jwt  from "jsonwebtoken";
+import jwt from "jsonwebtoken";
 import type { JwtPayload } from "jsonwebtoken";
 import config from "../config";
 import { pool } from "../db";
@@ -8,51 +8,57 @@ import { pool } from "../db";
 
 const auth = () => {
     return async (req: Request, res: Response, next: NextFunction) => {
-        // console.log('This is protected route');
-        // console.log(req.headers.authorization);
-        const token = req.headers.authorization;
 
-        console.log(token);
+        try {
+            // console.log('This is protected route');
+            // console.log(req.headers.authorization);
+            const token = req.headers.authorization;
 
-        if (!token) {
-            res.status(401).json({
-                success: false,
-                message: "Unauthorized access! "
-            })
+            console.log(token);
+
+            if (!token) {
+                res.status(401).json({
+                    success: false,
+                    message: "Unauthorized access! "
+                })
+            }
+
+            const decoded = jwt.verify(token as string,
+                config.secret as string
+            ) as JwtPayload;
+
+
+
+            const userData = await pool.query(`
+        SELECT * FROM users WHERE email = $1
+            `, [decoded.email])
+
+
+            const user = userData.rows[0]
+
+            if (userData.rows.length === 0) {
+                res.status(404).json({
+                    success: false,
+                    message: "User not found",
+                })
+            }
+
+
+            if (!user.is_active) {
+                res.status(403).json({
+                    success: false,
+                    message: "Forbidden!!"
+                })
+            }
+
+            req.user = decoded; 
+
+
+            next();
+        } catch (error) {
+            next(error);
         }
 
-        const decode = jwt.verify(token as string,
-            config.secret as string
-        )  as JwtPayload;
-    
-
-
-        const userData = await pool.query(`
-        SELECT * FROM users WHERE email = $1
-            `, [decode.email])
-        
-
-        const user = userData.rows[0]
-
-       if(userData.rows.length === 0) {
-            res.status(404).json({
-                success : false,
-                message :  "User not found",
-            })
-       }
-
-
-       if(!user.is_active) {
-            res.status(403).json({
-            success : false,
-            message : "Forbidden!!"  
-            })
-       }
-
-
-       next();
-
-        
     };
 }
 
